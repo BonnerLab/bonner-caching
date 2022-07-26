6aa0d22aeaad483813777baf34c2717b227ef3d5
Raj Magesh Gauthaman <raj.magesh@protonmail.com>
2022-07-26 16:51:16 -0400

    .

--- bonner/caching/_cacher.py
+++ bonner/caching/_cacher.py
@@ -71,7 +71,7 @@ class Cacher:
         return wrapper
 
     def is_stored(self, identifier: str) -> Path | None:
-        filepaths = list(self.cache_dir.glob(identifier))
+        filepaths = list(self.cache_dir.glob(f"{identifier}*"))
         assert len(filepaths) <= 1, "more than one file matches this identifier"
         if len(filepaths) == 1:
             return filepaths[0]
