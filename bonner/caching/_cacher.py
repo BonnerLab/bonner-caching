30a6c77feadf504c145f94098d5a94dafc34a5b1
Raj Magesh Gauthaman <raj.magesh@protonmail.com>
2022-09-13 15:36:06 -0400

    .

--- bonner/caching/_cacher.py
+++ bonner/caching/_cacher.py
@@ -1,16 +1,13 @@
+__all__ = ["Cacher"]
+
 from typing import Any, ParamSpec, TypeVar
 from collections.abc import Callable, Mapping
 from functools import wraps
-
-from tqdm.dask import TqdmCallback
 import inspect
 from pathlib import Path
 import os
-import pickle
 
-from loguru import logger
-import numpy as np
-import xarray as xr
+from ._handlers import get_handler
 
 P = ParamSpec("P")
 R = TypeVar("R")
@@ -23,11 +20,8 @@ DEFAULT_MODE = os.getenv("BONNER_CACHING_MODE", "normal")
 MODES = {"normal", "readonly", "overwrite", "delete", "ignore"}
 
 
-# TODO factorize the loaders and savers for different formats into their own classes; see bonner-brainio's Uploader/Downloader classes for inspiration
-
-
 class Cacher:
-    def __init__(
+    def __init__(  # type: ignore  # kwargs can be Any
         self,
         *,
         path: Path = DEFAULT_PATH,
@@ -36,7 +30,7 @@ class Cacher:
         filetype: str = "pickle",
         kwargs_save: Mapping[str, Any] = {},
         kwargs_load: Mapping[str, Any] = {},
-    ) -> None:
+    ) -> Callable[[Callable[P, R]], Callable[P, R]]:
         self.path = path
         self.path.mkdir(parents=True, exist_ok=True)
 
@@ -50,85 +44,62 @@ class Cacher:
 
     def __call__(
         self,
-        function: Callable[P, R],
+        func: Callable[P, R],
     ) -> Callable[P, R]:
-        @wraps(function)
+        @wraps(func)
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
-            call_args = self.get_args(function, *args, **kwargs)
+            call_args = self._get_args(func, *args, **kwargs)
 
             identifier = self.identifier.format(**call_args)
 
             if self.mode == "normal":
-                if self.is_stored(identifier):
+                if self._get_path(identifier):
                     result = self.load(identifier, **self.kwargs_load)
                 else:
-                    result = function(*args, **kwargs)
+                    result = func(*args, **kwargs)
                     self.save(result, identifier, **self.kwargs_save)
             elif self.mode == "readonly":
-                if self.is_stored(identifier):
+                if self._get_path(identifier):
                     result = self.load(identifier, **self.kwargs_load)
                 else:
-                    result = function(*args, **kwargs)
+                    result = func(*args, **kwargs)
             elif self.mode == "overwrite":
-                result = function(*args, **kwargs)
+                result = func(*args, **kwargs)
                 self.save(result, identifier, **self.kwargs_save)
             elif self.mode == "delete":
-                if self.is_stored(identifier):
+                if self._get_path(identifier):
                     self.delete(identifier)
-                result = function(*args, **kwargs)
+                result = func(*args, **kwargs)
             elif self.mode == "ignore":
-                result = function(*args, **kwargs)
+                result = func(*args, **kwargs)
             return result
 
         return wrapper
 
-    def is_stored(self, identifier: str) -> Path | None:
+    def save(self, result: Any, identifier: str) -> None:  # type: ignore  # result can be Any
+        path = self.path / identifier
+        path.parent.mkdir(parents=True, exist_ok=True)
+
+        handler = get_handler(filetype=self.filetype)
+        handler.save(result=result, path=path, **self.kwargs_save)
+
+    def load(self, identifier: str) -> Any:  # type: ignore  # file contents can be Any
+        path = self._get_path(identifier)
+        handler = get_handler(filetype=self.filetype)
+        return handler.load(path=path, **self.kwargs_load)
+
+    def delete(self, identifier: str) -> None:
+        filepath = self._get_path(identifier)
+        filepath.unlink()
+
+    def _get_path(self, identifier: str) -> Path | None:
         path = self.path / identifier
         if path.exists():
             return path
         else:
             return None
 
-    def save(self, result: Any, identifier: str, **kwargs_save) -> None:  # type: ignore  # result can be Any
-        if result is None:
-            return
-        path = self.path / identifier
-        path.parent.mkdir(parents=True, exist_ok=True)
-        if self.filetype == "numpy":
-            np.save(self.path / identifier, result, **kwargs_save)
-        elif self.filetype == "netCDF4":
-            if isinstance(result, xr.DataArray) and result.size == 0:
-                logger.warning(
-                    "The result has size 0, writing an empty netCDF4 file to"
-                    f" {self.path / identifier}"
-                )
-                x = xr.DataArray()
-                x.to_netcdf(self.path / identifier)
-            else:
-                with TqdmCallback(desc="dask", leave=False):
-                    result.to_netcdf(self.path / identifier, **kwargs_save)
-        elif self.filetype == "pickle":
-            with open(self.path / identifier, "wb") as f:
-                pickle.dump(result, f, **kwargs_save)
-
-    def load(self, identifier: str, **kwargs_load) -> Any:  # type: ignore  # file contents can be Any
-        filepath = self.is_stored(identifier)
-        if self.filetype == "numpy":
-            return np.load(filepath, **kwargs_load)
-        elif self.filetype == "netCDF4":
-            try:
-                return xr.open_dataarray(filepath, **kwargs_load)
-            except Exception:
-                return xr.open_dataset(filepath, **kwargs_load)
-        elif self.filetype == "pickle":
-            with open(filepath, "rb") as f:
-                return pickle.load(f, **kwargs_load)
-
-    def delete(self, identifier: str) -> None:
-        filepath = self.is_stored(identifier)
-        filepath.unlink()
-
-    def get_args(  # type: ignore  # arguments can be Any
+    def _get_args(  # type: ignore  # arguments can be Any
         self, function: Callable[P, R], *args: P.args, **kwargs: P.kwargs
     ) -> dict[str, Any]:
         signature = inspect.signature(function)
